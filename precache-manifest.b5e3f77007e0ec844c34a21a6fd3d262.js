1014c82a492a45b3af5eda0ac3dffad9e813483c
Pyatro <karotkavichy@gamail.com>
2020-02-10 19:58:53 +0300

    Updates

---
+++ precache-manifest.b5e3f77007e0ec844c34a21a6fd3d262.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "387751f1da83b0712fde8a3777e3b5a8",
+    "url": "/CyberAliosha-Parter-React/index.html"
+  },
+  {
+    "revision": "0fc33546a9d9249bb361",
+    "url": "/CyberAliosha-Parter-React/static/css/main.689e4427.chunk.css"
+  },
+  {
+    "revision": "559686a6be5b741601a2",
+    "url": "/CyberAliosha-Parter-React/static/js/2.63bc8f40.chunk.js"
+  },
+  {
+    "revision": "933f139c1acbc3546cc823b3421e06f7",
+    "url": "/CyberAliosha-Parter-React/static/js/2.63bc8f40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fc33546a9d9249bb361",
+    "url": "/CyberAliosha-Parter-React/static/js/main.d306f14d.chunk.js"
+  },
+  {
+    "revision": "96f86e966983d79d6f28",
+    "url": "/CyberAliosha-Parter-React/static/js/runtime-main.65ac1b0c.js"
+  }
+]);
