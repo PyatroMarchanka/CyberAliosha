5043feb62ef10b285163f5efef04434b126e8ea1
PETR_MARCHANKA <pmarchanka@i-deasoft.com>
2021-02-09 20:00:50 +0300

    add prefix for prod

--- src/components/global/AlioshaLink.tsx
+++ src/components/global/AlioshaLink.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { rootPrefix } from '../../utils/constants';
 
 interface Props {
   className?: string;
@@ -11,7 +12,7 @@ interface Props {
 
 export const AlioshaLink = styled(({ className, to, children, onClick }: Props) => {
   return (
-    <Link onClick={!!onClick ? onClick : undefined} className={className} to={to}>
+    <Link onClick={!!onClick ? onClick : undefined} className={className} to={rootPrefix + to}>
       {children}
     </Link>
   );
