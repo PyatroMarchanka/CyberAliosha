f98235dad5fa396cab6f0d901d5fdb9046289cb2
PETR_MARCHANKA <pmarchanka@i-deasoft.com>
2021-02-16 10:27:17 +0300

    add last bar in melody with new rythmic pattern

--- src/MidiFileCreater/PartCreator.js
+++ src/MidiFileCreater/PartCreator.js
@@ -6,6 +6,7 @@ export default class PartCreator {
     this.pattern =
       partOptions.pattern === 'accompaniment' &&
       new PatternCreator().getPattern(partOptions.notesLength, partOptions.type);
+    this.endPattern = new PatternCreator().getPattern(partOptions.notesLength, partOptions.type);
     this.chords = chords;
     this.barCreator = new BarCreator(partOptions.notesLength, partOptions.type);
     this.squaresCountToAdd = squaresCount;
@@ -13,8 +14,23 @@ export default class PartCreator {
     this.onInit(partOptions.restProbability / 100);
   }
 
+  createNewRandomPart(restProbability) {
+    for (let index = 0; index < this.squaresCountToAdd; index++) {
+      this.chords.forEach((chord, i) => {
+        const newBar = this.barCreator.getRandomBar(
+          chord,
+          i,
+          (i + 1) % 4 === 0 ? this.endPattern : this.pattern,
+          restProbability,
+        );
+
+        this.notes = [...this.notes, newBar];
+      });
+    }
+  }
+
   onInit(restProbability) {
-    this.createRandomPartBars(restProbability);
+    this.createNewRandomPart(restProbability);
   }
 
   createRandomPart(restProbability) {
