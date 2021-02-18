045f2c6e7e69ed4207698ab3dc07603543dabbfb
PETR_MARCHANKA <pmarchanka@i-deasoft.com>
2021-02-18 10:17:45 +0300

    Updates

---
+++ precache-manifest.d222841ab6e28b1f63aeab55b634761a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce6a44b10bcb1fe54c0adbdab147c1aa",
+    "url": "/CyberAliosha-Parter-React/index.html"
+  },
+  {
+    "revision": "fcbd099a84bfc00b86c2",
+    "url": "/CyberAliosha-Parter-React/static/css/main.e5416ec8.chunk.css"
+  },
+  {
+    "revision": "23cab0693559536a404e",
+    "url": "/CyberAliosha-Parter-React/static/js/2.526b244b.chunk.js"
+  },
+  {
+    "revision": "9068216d0bb8bca50153cf4320e81845",
+    "url": "/CyberAliosha-Parter-React/static/js/2.526b244b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcbd099a84bfc00b86c2",
+    "url": "/CyberAliosha-Parter-React/static/js/main.b68cfd96.chunk.js"
+  },
+  {
+    "revision": "96f86e966983d79d6f28",
+    "url": "/CyberAliosha-Parter-React/static/js/runtime-main.65ac1b0c.js"
+  },
+  {
+    "revision": "8b0b9deb32db45dc4e0fb72cddc64914",
+    "url": "/CyberAliosha-Parter-React/static/media/HelveticaComp.8b0b9deb.ttf"
+  }
+]);
